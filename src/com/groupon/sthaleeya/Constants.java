39bd7415c1246f0697f1f5ca1fcbac68d58c5d87
nizhashree <nizhashree@gmail.com>
2012-12-11 11:16:39 +0530

    adding user to server while login

--- src/com/groupon/sthaleeya/Constants.java
+++ src/com/groupon/sthaleeya/Constants.java
@@ -82,4 +82,5 @@ public class Constants {
     public static final String KEY_REFRESH_RATE = "refreshRate";
     
     public static final String SERVER_URL = "http://10.1.23.243/sthaleeya_all.php";
+    public static final String ADD_USER_URL = "http://10.1.23.243/add_user.php";
 }
